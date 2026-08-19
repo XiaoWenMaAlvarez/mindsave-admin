adcc690ae3367c4b61484345d54875b318cc49ed
Xiao Ma <xi.ma@duocuc.cl>
2026-08-19 16:32:09 -0400

    Inicialización del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className='titulo'>Hola mundo</h1>
+  )
+}
+
+export default App
---
+++ tests/app.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test, expect } from 'vitest';
+
+describe("prueba", () => {
+
+  test("Should pass", () => {
+    expect(1+1).toBe(2)
+  })
+
+})
